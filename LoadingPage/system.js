8a9ac1d4e853df4859ae63ba5f18385d2f2d95de
oskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-01-25 01:21:04 +0700

    Update 25 Jun 2023

--- LoadingPage/system.js
+++ LoadingPage/system.js
@@ -1,17 +1,33 @@
-// Variable
-var udah = false;
+// VARIABLE
+var sudah = false;
 
-// Method
+// FUNCTION
 function loop() {
-  // Untuk mengganti nama element dari box setiap setengah putaran
-  if (udah) {
-    document.querySelector('#latar').innerHTML = '<div class="box box1"><div></div></div><div class="box box2"><div></div></div><div class="box box3"><div></div></div><div class="box box4"><div></div></div><div class="box box5"><div></div></div>';
-    udah = false;
-  }else{
-    document.querySelector('#latar').innerHTML = '<div class="boxN boxN1"><div></div></div><div class="boxN boxN2"><div></div></div><div class="boxN boxN3"><div></div></div><div class="boxN boxN4"><div></div></div><div class="boxN boxN5"><div></div></div>';
-    udah = true;
-  }
+    if (sudah) {
+
+        document.getElementById("latar").innerHTML = `
+        <div class="box box1"><div></div></div>
+        <div class="box box2"><div></div></div>
+        <div class="box box3"><div></div></div>
+        <div class="box box4"><div></div></div>
+        <div class="box box5"><div></div></div>
+        `;
+
+        sudah = false;
+
+    } else {
+
+        document.getElementById("latar").innerHTML = `
+        <div class="boxN boxN1"><div></div></div>
+        <div class="boxN boxN2"><div></div></div>
+        <div class="boxN boxN3"><div></div></div>
+        <div class="boxN boxN4"><div></div></div>
+        <div class="boxN boxN5"><div></div></div>
+        `;
+
+        sudah = true;
+
+    }
 }
 
-// Memulai fungsi agar berjalan secara berulang ulang
 setInterval(loop, 2500);
